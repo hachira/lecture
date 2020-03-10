53bd39dd624da7db242ba23986173136f7bd0e18
hachira <fortune@dongguk.edu>
2020-03-10 16:57:19 +0900

    data structure.

---
+++ 2020/ds/list.java
@@ -0,0 +1,7 @@
+class Main {
+	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+		System.out.println("Elapsed : " + (System.currentTimeMillis()-start)/1000.0);
+	}
+}
+

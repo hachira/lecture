e5000c3e615ef23e51cb71f59be3578f11baf922
hachira <fortune@dongguk.edu>
2020-09-03 14:31:40 +0900

    Change for exam.

--- 2020/datascience/3-3.py
+++ 2020/datascience/3-3.py
@@ -7,12 +7,10 @@ soup = BeautifulSoup(html, "html.parser")
 #print(soup.prettify())
 #print(soup.get_text())
 
-"""
 #   <pre> tag를 찾아서 pres에 저장한다.
 pres = soup.find_all("pre")
 for s in pres:
     print(s)
-"""
 
 #   모든 링크를 가져오자.
 links = [(link.string, link["href"])
